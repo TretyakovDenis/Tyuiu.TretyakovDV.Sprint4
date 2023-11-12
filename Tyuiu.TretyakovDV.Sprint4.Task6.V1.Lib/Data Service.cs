2d3a254f65ffca863b40dcfe1d752872c348e303
Tyuiu.TretyakovDV@outlook.com <trev1793@gmail.com>
2023-11-12 21:32:59 +0500

    task6 fix

--- Tyuiu.TretyakovDV.Sprint4.Task6.V1.Lib/Data Service.cs
+++ Tyuiu.TretyakovDV.Sprint4.Task6.V1.Lib/Data Service.cs
@@ -8,7 +8,7 @@ using tyuiu.cources.programming.interfaces.Sprint4;
 
 namespace Tyuiu.TretyakovDV.Sprint4.Task6.V1.Lib
 {
-    public class DataService : ISprint4Task6V0
+    public class DataService : ISprint4Task6V1
     {
         public int Calculate(string[] array)
         {
